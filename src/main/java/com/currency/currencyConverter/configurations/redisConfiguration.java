eef87050778a432cf5dcf3bfb3cd427ddcdeac77
Deipey Paanchal <deipey009@gmail.com>
2022-09-29 17:35:30 -0400

    Commit

---
+++ src/main/java/com/currency/currencyConverter/configurations/redisConfiguration.java
@@ -0,0 +1,18 @@
+package com.currency.currencyConverter.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
+
+@Configuration
+public class redisConfiguration {
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(new JedisConnectionFactory());
+        template.setValueSerializer(new GenericToStringSerializer<Object>(Object.class));
+        return template;
+    }
+}
---
+++ src/main/java/com/currency/currencyConverter/models/Currency.java
@@ -0,0 +1,4 @@
+package com.currency.currencyConverter.models;
+
+public class Currency {
+}
